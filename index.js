105d6b15c3e0f6e5c3c8f0468637ca723ed9230b
Diego Torres <diegotc86@gmail.com>
2025-11-12 21:49:40 -0500

    Agrega EJS

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ const app = express();
 // Static file serving middleware
 app.use(express.static("public"));
 
+app.set("view engine", "ejs");
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    nombre: "Codeable",
+    tecnologias: ["HTML", "CSS", "JS"],
+  });
+});
+
 app.use((req, res, next) => {
   res.status(404).sendFile("404.html", { root: "public" });
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000`);
 });
